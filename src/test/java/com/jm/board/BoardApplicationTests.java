609a6ed53f0368d7b0250d17247b8fef42d31b33
jm <j_all@naver.com>
2023-06-24 21:35:23 +0900

    test

---
+++ src/test/java/com/jm/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
